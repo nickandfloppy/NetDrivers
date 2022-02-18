a28a7587c8a7b168a6f0c10bcb75a55b616a4a33
Frankie B <floppydisk@nick99nack.com>
2022-02-18 23:46:24 +0000

    Add existing files

---
+++ syssearch.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>System Search</title>
+    <link rel="shortcut icon" type="image/png" href="/favicon.png"/>
+</head>
+<body>
+<h1>System Search</h1>
+<a href="/">Return to dev menu</a>
+<hr>
+<form action="systemsearch.php" method="post" target="_parent">
+    Query: <input type="text" name="query"><br>
+    <input type="submit">
+</form>
+</body>
+</html>
