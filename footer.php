650bbf15fe0142122b98b533fee40e8033adb13c
Frankie B <floppydisk05@aol.com>
2022-02-26 23:47:59 +0000

    Make footer one line and update image links in stylesheet

--- footer.php
+++ footer.php
@@ -1,2 +1,1 @@
-<?php
-echo "<p class=\"copyright\"><i>Copyright <a href=\"https://nickandfloppy.com/\">nick and floppy</a> " . date("Y"). "</i></p>";
+<?php echo "<p class=\"copyright\"><i>Copyright <a href=\"https://nickandfloppy.com/\">nick and floppy</a> " . date("Y"). "</i></p>"; ?>
