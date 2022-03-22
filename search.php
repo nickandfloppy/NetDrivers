4c61325059c93b853258af35d6a6b15a84ec5c42
Frankie B <floppydisk05@aol.com>
2022-03-22 00:38:07 +0000

    Added the option to search by filename

--- search.php
+++ search.php
@@ -12,8 +12,10 @@
 		<input type="radio" name="scope" checked="true"
 <?php if (isset($scope) && $scope=="systems") echo "checked";?>
 value="systems">Systems<input type="radio" name="scope"
+<?php if (isset($scope) && $scope=="devices") echo "checked";?>
+value="devices">Devices<input type="radio" name="scope"
 <?php if (isset($scope) && $scope=="drivers") echo "checked";?>
-value="drivers">Drivers
+value="drivers">Driver Filename
 </form>
 <br>
 <?php
@@ -28,10 +30,9 @@ $queryScope = test_input($_POST["scope"]);
 $query = '%'.$_POST["query"].'%';
 if($query != "%%"){
 	$cleanquery = str_replace("%","",$query);
-	echo "Results for \"" . $cleanquery . "\" in " . $queryScope;
+	//echo "Results for \"" . $cleanquery . "\" in " . $queryScope;
 }
 ?>
-<hr>
 <?php
 include 'creds.php';
 
@@ -43,14 +44,15 @@ if ($conn->connect_error) {
 }
 
 if ($queryScope == "systems") {
-	if ($query == "%%"){
+	// Query by system model
+	if ($query == "%%") {
 		return;
-	}else{
+	} else {
 		$stmt = $conn->prepare("SELECT ID, Manufacturer, Model FROM systems WHERE Model LIKE ?");
 		$stmt->bind_param(s,$query);
 		$stmt->execute();
 		$result = $stmt->get_result();
-	
+		echo $result->num_rows . " results for \"" . $cleanquery . "\" in " . $queryScope . "<hr>";
 	}
 	
 	if ($result->num_rows > 0) {
@@ -60,27 +62,49 @@ if ($queryScope == "systems") {
 			echo "<hr>";
 		}
 	} else {
-		echo "No Results for " . $query;
+		echo "No Results for " . $cleanquery;
 	}
-} else if ($queryScope == "drivers") {
-    if ($query == "%%"){
+} else if ($queryScope == "devices") {
+	// Query by device name/manufacturer
+    if ($query == "%%") {
 		return;
-	}else{
-		$stmt = $conn->prepare("SELECT ID, Manufacturer, Device_Name FROM drivers WHERE Device_Name LIKE ? OR Manufacturer LIKE ?");
+	} else {
+		$stmt = $conn->prepare("SELECT ID, Manufacturer, Device_Name FROM devices WHERE Device_Name LIKE ? OR Manufacturer LIKE ?");
 		$stmt->bind_param(ss,$query, $query);
 		$stmt->execute();
 		$result = $stmt->get_result();
-	
+		echo $result->num_rows . " results for \"" . $cleanquery . "\" in " . $queryScope . "<hr>";
 	}
 	
 	if ($result->num_rows > 0) {
 		// output data of each row
 		while($row = $result->fetch_assoc()) {
-			echo "<h2><a href=\"/drivers.php?id=" . $row["ID"] . "\">". $row["Manufacturer"] . " " . $row["Device_Name"] . "</a></h2>";
+			echo "<h2><a href=\"/devices.php?id=" . $row["ID"] . "\">". $row["Manufacturer"] . " " . $row["Device_Name"] . "</a></h2>";
 			echo "<hr>";
 		}
 	} else {
 		echo "No Results for \"" . $cleanquery . "\"";
+	}
+} else if ($queryScope == "drivers") {
+	// Query by driver filename
+	if ($query == "%%") {
+		return;
+	} else {
+		$stmt = $conn->prepare("SELECT ID, File_Name, File_Path, Version FROM drivers WHERE File_Name LIKE ?");
+		$stmt->bind_param(s,$query);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		echo $result->num_rows . " results for \"" . $cleanquery . "\" in driver files<hr>";
+		if ($result->num_rows > 0) {
+			// output data of each row
+			while($row = $result->fetch_assoc()) {
+				$date = new DateTime($row["Date"]);
+				echo "<p><b>Filename:</b> ". $row["File_Name"] . "<br><b>Version:</b> " . $row["Version"] . "<br><b>Date:</b> " . $date->format("d M Y") . "<br><a href=\"/file.php?id=" . $row["ID"] . "\"><button type=\"button\">Download</button></a></p>";
+				echo "<hr>";
+			}
+		} else {
+			echo "No Results for \"" . $cleanquery . "\"";
+		}
 	}
 }
 $conn->close();
