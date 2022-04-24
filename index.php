51d5abbbe50516f55606827801004d88e85d07eb
Animadoria <me@animadoria.cf>
2022-04-25 00:19:21 +0100

    Index: use require instead of include, use single quotes, general fixes

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
-<html>
+<?php
+declare(strict_types=1);
+?>
 <head>
-	<?php $title="Home"; include 'head.php'; ?>
+   <?php
+   $title = 'Home';
+
+   require('head.php'); ?>
 </head>
 
 <body>
-	<a href="/"><table><tr>
-		<td><img src="/favicon.png" width="50"></td>
-		<td><h1 style="margin: 0">NetDrivers</h1><i>Archiving Drivers Since February 2022</i></td>
-	</tr></table></a>
-	<hr>
-    <?php include 'nav.html'; ?>
-    <hr>
-	<p>This page currently has no content. Use the above links to navigate the site!</p>
-	<hr>
-	<address>&copy; 2022 <a href="http://nickandfloppy.com/">nick and floppy</a></address>
+<a href="/">
+	<table>
+		<tr>
+			<td><img src="/favicon.png" width="50"></td>
+			<td><h1 style="margin: 0">NetDrivers</h1><i>Archiving Drivers Since February 2022</i></td>
+		</tr>
+	</table>
+</a>
+<hr>
+<?php require('nav.html'); ?>
+<hr>
+<p>This page currently has no content. Use the above links to navigate the site!</p>
+<hr>
+<address>&copy; 2022 <a href="http://nickandfloppy.com/">nick and floppy</a></address>
 </body>
 </html>
