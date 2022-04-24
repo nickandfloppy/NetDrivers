7004db7afe8f1be456e146a60aa23aa6ff20f2d2
Animadoria <me@animadoria.cf>
2022-04-25 00:18:21 +0100

    err.php: ini_set requires a string, not an integer

--- err.php
+++ err.php
@@ -1,6 +1,7 @@
 <?php
+declare(strict_types=1);
 // Include this file to display errors when they are encountered
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 ?>
