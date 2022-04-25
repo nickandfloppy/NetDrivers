aedad02f2852606b3f6e451d3cf62997c45d857e
dkay0670 <63021402+dkay0670@users.noreply.github.com>
2022-04-24 17:09:38 -0700

    Update head.php
    Fix minor escaping issue for favicon link

--- head.php
+++ head.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 ?>
 
 <link rel="stylesheet" href="/src/style.css">
-<link rel=\"icon\" type=\"image/png\" href=\"/favicon.png\">
+<link rel="icon" type="image/png" href="/favicon.png">
 <title>NetDrivers
    <?php
    if (isset($title)) {
