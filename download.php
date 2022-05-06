9404dd0c127b8dc4cedbf2ec3bd9025b224d20d7
Vbc <viniciusca260@gmail.com>
2022-05-06 00:00:27 -0300

    Fix download.php

--- download.php
+++ download.php
@@ -25,41 +25,42 @@ if (isset($_GET['id'])) {
    $stmt->execute();
    $result = $stmt->get_result();
 
-   $mirrors = 1;
-   $mirr_res = $conn->query('SELECT value FROM stats WHERE id=4');
-   $mirrors = $mirr_res->fetch_all(MYSQLI_ASSOC)[0]['value'];
-   // Get mirror info
-   $sql = 'SELECT id, name, region, address, base_url, https FROM mirrors';
-   $mirr_res = $conn->query($sql);
-
-   if ($mirrors > 0) {
+   if ($result->num_rows > 0) {
       //echo "<table border=\"1\"><td><b>Linkback: </b><a href=\"http://drivers.nickandfloppy.com/link.php?type=driver&id=" . $_GET['id'] . "\">http://drivers.nickandfloppy.com/link.php?type=driver&id=" . $_GET['id'] . "</td></table>";
-
+   
       // output data of each row
       foreach ($result->fetch_all(MYSQLI_ASSOC) as $row) {
+         $fileMirrors = json_decode($row['mirrors'], true, 512, JSON_THROW_ON_ERROR);
          // Commented out as it doesn't get used anywhere
          //$fileurl = $row['File_URL'] === null || $row['File_URL'] === '' ? 'N/A' : $row['File_URL'];
 
+         if (count($fileMirrors) < 1) {
+            echo 'No mirrors available';
+            continue;
+         }
          echo '<h1>Downloading ' . $row['file_name'] . ' (ID ' . urlencode($_GET['id']) .')</h1>';
          //echo '<b>Version:</b> ' . $row['version'] . '<br>';
          //echo '<b>Date:</b> ' . $row['date'] . '<br>';
-         $mirrors = json_decode($row['mirrors'], true, 512, JSON_THROW_ON_ERROR);
+         
          echo '<b>Select from the following mirrors...</b><br>';
-         foreach ($mirr_res->fetch_all(MYSQLI_ASSOC) as $mirrorRW) {
-            if (in_array($mirrorRW["id"], $mirrors, true)) {
-               $url = $mirrorRW['address'] . $mirrorRW['base_url'] . $row['file_path'] . $row['file_name'];
-               echo '<a href="' . ($mirrorRW['https'] === true ? 'https' : 'http') . '://' . $url . '">Mirror '
-                  . $mirrorRW['id'] . ' (' . $mirrorRW['region'] . ')</a><br>';
+         foreach($fileMirrors as $fileMirror) {
+            $mirrorStmt = $conn->prepare('SELECT id, name, region, address, base_url, https FROM mirrors WHERE id = ?');
+            $mirrorStmt->bind_param('i', $fileMirror);
+            $mirrorStmt->execute();
+            $mirrorResult = $mirrorStmt->get_result();
+            if ($mirrorResult->num_rows > 0) {
+               foreach($mirrorResult->fetch_all(MYSQLI_ASSOC) as $mirrorRow) {
+                  $url = $mirrorRow['address'] . $mirrorRow['base_url'] . $row['file_path'] . $row['file_name'];
+                  echo '<a href="' . ($mirrorRow['https'] === true ? 'https' : 'http') . '://' . $url . '">Mirror '
+                     . $mirrorRow['id'] . ' (' . $mirrorRow['region'] . ')</a><br>';
+               }
             }
          }
       }
    } else {
-      if ($mirrors > 0) {
-         echo 'Invalid ID provided';
-      } else {
-         echo 'No mirrors available';
-      }
+      echo 'Invalid ID provided';
    }
+   
    $conn->close();
 } else {
    echo 'No ID provided';
