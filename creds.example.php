69c756bb3600606ab47c7c5bef096086041b48ad
Frankie B <floppydisk05@aol.com>
2022-06-18 00:00:41 +0100

    Rename example creds.php file

---
+++ creds.example.php
@@ -0,0 +1,10 @@
+<?php
+// Create a constant array with credentials
+const CONF = array(
+    "servername" => "servername",
+    "username" => "dbusername",
+    "password" => "dbpassword",
+    "dbname" => "databasename",
+    "webhook" => "webhookurl"
+);
+?>
